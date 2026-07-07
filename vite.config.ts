0d1538290e3312967c996f02247e10bee606c277
vtn25022002 <157345220+vtn25022002@users.noreply.github.com>
2026-07-07 10:46:50 +0700

    Revert "feat/user_profile"
    This reverts commit 7f40edb0ec40b3b4d0cf8645efecef3962b5a8b1.

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        // target: 'http://localhost:8080',
-        target: 'https://greenslot-backend.onrender.com',
+        target: 'http://localhost:8080',
+        // target: 'https://greenslot-backend.onrender.com'
         changeOrigin: true,
         secure: true,
       },
